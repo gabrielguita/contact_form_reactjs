a82aa816fd397e110a94874694e332a58f9553da
Gabriel <ionut.guita@gmail.com>
2017-03-19 22:01:08 +0100

    initial commit react js shoping list

--- app/index.js
+++ app/index.js
@@ -1,135 +1,76 @@
 import React, { PropTypes } from 'react';
 import ReactDom from 'react-dom';
-import {inputField, submitButton, row, contactForm, contactFormWrapper} from './style.scss';
+import {header, productList, orderDetails, orderStatus, printBtn, btnBack, btnContact, btnDetails, contactDetails, checkbox, content, contactFormWrapper, photo, description, shopingListContainer, inputField, submitButton, row, contactForm} from './style.scss';
 
-//converting component
+var shopItems = require('json!./data/data.json');
 
-//after this routing
+var time = new Date(shopItems.data.assignedAt);
+var hour = time.getHours();
+var minutes = time.getMinutes();
+var getDay = time.getDate("dd-m-yy");
+var getMonth = time.getMonth("dd-m-yy");
+var getYear = time.getFullYear("dd-m-yy");
+var shopingItems = shopItems.data.items;
 
-//sync info
-
-
-// New file Container
-const Form = React.createClass({
- /*
-     ** import and export es6
-     Declaration
-     Anathomy of a component...
-     JSX: apply HTML of Javascript...
-     Mandatory.... render mothod...
-       Properties: Not mutable, they wont change...
-       State: what it mutable... what makes the UI RE-RENDER
-       setState({prop: value})
-       propTypes
-
-       StateLess Component is...
- */
- propTypes: {
-   name: PropTypes.string.isRequired,
-   lastName: PropTypes.string.isRequired,
- },
-
- getInitialState () {
-   return {
-     contact: {
-       name: '',
-       lastName: '',
-       message: '',
-       work: '',
-       sex: {
-         male: 'male',
-         female: 'female'
-       }
-     },
+var ShopingElements = React.createClass({
+ getDefaultProps: function() {
+  return {
+    shopingItems: [],
    }
  },
- handleClick (e) {
-   // e.preventDefault()
-   let field = e.target.name
-   let contact = this.state.contact
-   contact[field] = e.target.value
-   this.setState({contact}, function () {
-    console.log(this)
-   })
-     console.log('clicked!!!')
-   // this.setState({'name': 'Gabriel', 'lastName': 'from Ireland'})
- },
- handleRadioChange (e) {
-     console.log(e.target.value)
-     let contact = this.state.contact;
-     contact['work'] = e.target.value
-     this.setState({contact})
-},
-
- handleSubmit (e) {
-  console.log(this.state.contact)
-  e.preventDefault();
- },
- render () {
+ render: function(color) {
+  var listItems = this.props.shopingItems.map(function(shopingItem) {
    return (
-       <FormChild handleClick={this.handleClick} contact={this.state.contact} handleSubmit={this.handleSubmit} handleRadioChange={this.handleRadioChange}/>
-   )
- }
-})
-
-// Component Presentation
-const FormChild = ({handleClick, contact, handleSubmit, handleRadioChange}) => {
-console.log(contact);
- return (
-    <div className={contactForm}>
-         {`Hi, I'm a component, / my name is: ${contact["name"]} / my last name is:  ${contact["lastName"]} / this is my message: ${contact["message"]}`}
-      <div className={contactFormWrapper}>
-        <label className={row}>
-            Your first name:  <input className={inputField} onChange={handleClick} name='name' value={contact['name']} type='text' placeholder="your name" />
-        </label>
-         <br />
-        <label className={row}>
-          Your last name:
-          <input className={inputField} onChange={handleClick} name='lastName' value={contact['lastName']} type='text' placeholder="your last name" />
-        </label>
-      <br />
-  <label htmlFor="work">
-    <input
-    type="radio"
-    name="work"
-    value="male"
-    checked={contact['work'] === 'male'}
-    onChange={handleRadioChange}
-  />
- Public</label>
-
- <label htmlFor="personal"> <input
-    type="radio"
-    name="work"
-    value="female"
-    checked={contact['work'] === 'female'}
-    onChange={handleRadioChange}
-  />
-  Private</label>
-         <br/>
-        <label className={row}>
-          Your sex:
-          <select className={inputField} onChange={handleClick} name='sex' value={contact['sex']}>
-            <option value="">Select an option</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="noSex">No Sex</option>
-          </select>
-        </label>
-         <br />
-        <label className={row}> Your message:
-          <textarea className={inputField} onChange={handleClick} name='message' value={contact['message']}  placeholder="your message"  /><br />
-        </label>
-         <br />
-        <input className={submitButton} onClick={handleSubmit} type='submit' value='submit' /><br />
+    <li key={shopingItem.id}>
+      <div className={checkbox}>
       </div>
-    </div>
- )
-}
+      <div className={content}>
+        <img className={photo} src={shopingItem.product.imageUrl} />
+        <div className={description}>
+          <h3>{shopingItem.product.name}</h3>
+          <p>{shopingItem.product.description}</p>
+        </div>
+        <span className={btnDetails}></span>
+      </div>
+    </li>
+   );
+  });
 
-FormChild.propTypes = {
- handleClick: PropTypes.func.isRequired,
- contact: PropTypes.object,
-}
+  return (
+      <div className={shopingListContainer}>
+        <div className={header}>
+            <span className={btnBack}></span>
+            <h3>{shopItems.data.assignee.name} </h3>
+            <p>{shopItems.data.assignee.id}</p>
+            <span className={printBtn}></span>
+        </div>
+        <div className={orderDetails}>
+          <ul>
+            <li><strong>Aangevraagd om:</strong> {hour}.{minutes} ({getDay}-{getMonth}-{getYear})</li>
+            <li><strong>Email:</strong> {shopItems.data.customer.email}</li>
+            <li><strong>Telefoon:</strong> {shopItems.data.customer.phoneNumber}</li>
+          </ul>
+        </div>
+        <div className={orderStatus}>
+          <table>
+            <tr>
+              <td>Status</td>
+              <td>Door</td>
+              <td>Om</td>
+            </tr>
+            <tr>
+              <td>{shopItems.data.status}</td>
+              <td>{shopItems.data.assignee.name}</td>
+              <td>{shopItems.data.assignedAt}</td>
+            </tr>
+          </table>
+        </div>
+       <ul className={productList}>
+        {listItems}
+       </ul>
+      </div>
+  );
+ }
+});
 
-ReactDom.render( <Form />, document.getElementById("app"))
+ReactDom.render( <ShopingElements shopingItems={shopingItems} />, document.getElementById("app"))
